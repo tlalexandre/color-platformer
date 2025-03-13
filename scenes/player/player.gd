2501d5457b385287a20fbe6bb5008a53ba6d5609
BanditCook <tanguy.lalexandre@gmail.com>
2025-03-13 19:14:32 +0000

    Added sprite, animation to player character

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var body: Node2D = $Body
 
 @export var GRAVITY: float =  700.0
 @export var speed: float = 300.0
 @export var jump_strength: float = -300.0
+@export var body_scale: float = 0.15
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -19,15 +22,33 @@ func _physics_process(delta: float) -> void:
 
 
 func handle_movement() -> void:
-	# Horizontal movement
+	# Determine horizontal movement
 	if Input.is_action_pressed("left"):
 		velocity.x = -speed
+		body.scale.x = -body_scale
 	elif Input.is_action_pressed("right"):
 		velocity.x = speed
+		body.scale.x = body_scale
 	else:
 		velocity.x = 0
 	
-	# Jumping
+	# Handle jumping input
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = jump_strength
+	
+	# Determine which animation to play based on character state
+	if not is_on_floor():
+		if velocity.y < 0:
+			# Rising/jumping
+			animation_player.play("jump")
+		else:
+			# Falling
+			animation_player.play("fall")  # Create a fall animation or use jump
+	elif velocity.x != 0:
+		# Walking on ground
+		animation_player.play("walk")
+	else:
+		# Standing still
+		animation_player.play("idle")
+	
 		
--- scenes/scripts/IPerspective.gd
+++ scenes/scripts/IPerspective.gd
@@ -2,15 +2,29 @@
 extends Node
 class_name IPerspective
 
-# Define the interface methods that classes need to implement
+# Define the interface methods with smarter default implementations
 func get_render_bounds() -> Rect2:
-	push_error("IPerspective: get_render_bounds not implemented")
+	var parent = get_parent()
+	if not parent:
+		push_error("IPerspective: No parent node found")
+		return Rect2()
+	
+	# Try to find any collision shape in order of priority
+	if parent.has_node("BackgroundSprite"):
+		var sprite = parent.get_node("BackgroundSprite")
+		var sprite_size = sprite.texture.get_size() * sprite.scale
+		return Rect2(sprite.position, sprite_size)
+	# 1. Try standard barrier collision shape
+
+	# If we got here, we couldn't find a suitable shape
+	push_error("IPerspective: Could not find a suitable collision shape for " + parent.name)
 	return Rect2()
 
 func get_render_offset() -> Vector2:
-	push_error("IPerspective: get_render_offset not implemented")
+	var bounds = get_render_bounds()
+	if bounds != Rect2():
+		return bounds.position
 	return Vector2.ZERO
 
 func get_render_depth() -> float:
-	push_error("IPerspective: get_render_depth not implemented")
 	return 0.5
